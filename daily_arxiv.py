e06742f79ca4ced01d7b484f309005c1beedaf42
Youdon <axyzdong@gmail.com>
2026-05-24 19:43:00 +0800

    Refactor search engine to use arxiv.Client

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -111,7 +111,9 @@ def get_daily_papers(topic, query=" ", max_results=2):
         sort_by=arxiv.SortCriterion.SubmittedDate
     )
 
-    for result in search_engine.results():
+    client = arxiv.Client()
+  
+    for result in client.results(search_engine):
 
         paper_id = result.get_short_id()
         paper_title = result.title
